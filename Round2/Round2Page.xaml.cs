0bab7d778b980e224e13743c192ce48934dd4dac
June Cho <junecho.ucb@gmail.com>
2018-01-24 19:28:12 -0800

    intial commit

---
+++ Round2/Round2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Round2
+{
+    public partial class Round2Page : ContentPage
+    {
+        public Round2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
