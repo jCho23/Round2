d98241f16f61b8c1a8d1ad3eb24bfa92b61ba25a
June Cho <junecho.ucb@gmail.com>
2018-01-26 20:17:21 -0800

    StackLayout

---
+++ Round2/Pages/StackLayout.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Round2.Pages
+{
+    public partial class StackLayout : ContentPage
+    {
+        public StackLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
